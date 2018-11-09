5f2a2984f00a2adbe6b82ced07745518138c8a52
Martin <mjmenadev@gmail.com>
2018-11-08 19:44:05 -0500

    Updates

---
+++ precache-manifest.6879406e78fa76c1b607db1c3d324cf2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "97bd619ef3b51508df45",
+    "url": "/stardew-agenda/static/js/runtime~main.97bd619e.js"
+  },
+  {
+    "revision": "1b92854824a40239e1f7",
+    "url": "/stardew-agenda/static/js/main.1b928548.chunk.js"
+  },
+  {
+    "revision": "ab03eb65b4691bdc99c8",
+    "url": "/stardew-agenda/static/js/1.ab03eb65.chunk.js"
+  },
+  {
+    "revision": "1b92854824a40239e1f7",
+    "url": "/stardew-agenda/static/css/main.9fc006b8.chunk.css"
+  },
+  {
+    "revision": "a7a91f6863ec414c066704b47cef60ed",
+    "url": "/stardew-agenda/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -11,10 +11,10 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/stardew-agenda/precache-manifest.f3596898cc74e7c00d428aafabe9cf02.js"
+  "/stardew-agenda/precache-manifest.6879406e78fa76c1b607db1c3d324cf2.js"
 );
 
 workbox.clientsClaim();
